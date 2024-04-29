8af090c006cc6b73c07f39f9d199e714488ff484
lucasezidro <lucasezidro7@gmail.com>
2024-04-29 14:50:26 -0300

    add fake data to unit test dont use database

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,12 +1,25 @@
 import { expect, describe, it } from 'vitest'
 import { RegisterUseCase } from './register'
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 import { compare } from 'bcryptjs'
 
 describe('Register Use Case', () => {
   it('should hash user password upon registration', async () => {
-    const prismaUsersRepository = new PrismaUsersRepository()
-    const registerUseCase = new RegisterUseCase(prismaUsersRepository)
+    const registerUseCase = new RegisterUseCase({
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      async findByEmail(email) {
+        return null
+      },
+
+      async create(data) {
+        return {
+          id: 'user-1',
+          name: data.name,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date(),
+        }
+      },
+    })
 
     const { user } = await registerUseCase.execute({
       name: 'John Doe',

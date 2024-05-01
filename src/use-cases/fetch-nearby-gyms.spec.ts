936dfe362f7739b2b3897a1024c53ed48bfd7e54
lucasezidro <lucasezidro7@gmail.com>
2024-05-01 10:05:35 -0300

    fetch nearby gyms

---
+++ src/use-cases/fetch-nearby-gyms.spec.ts
@@ -0,0 +1,39 @@
+import { expect, describe, it, beforeEach } from 'vitest'
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { FetchNearbyGymsUseCase } from './fetch-nearby-gyms'
+
+let gymsRepository: InMemoryGymsRepository
+let sut: FetchNearbyGymsUseCase
+
+describe('Fetch Nearby gyms use case', () => {
+  beforeEach(() => {
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new FetchNearbyGymsUseCase(gymsRepository)
+  })
+
+  it('should be able to fetch nearby gyms', async () => {
+    await gymsRepository.create({
+      title: 'Near Gym',
+      description: null,
+      phone: null,
+      latitude: -23.7436928,
+      longitude: -46.4748544,
+    })
+
+    await gymsRepository.create({
+      title: 'Far Gym',
+      description: null,
+      phone: null,
+      latitude: -21.1987663,
+      longitude: -50.5435949,
+    })
+
+    const { gyms } = await sut.execute({
+      userLatitude: -23.7436928,
+      userLongitude: -46.4748544,
+    })
+
+    expect(gyms).toHaveLength(1)
+    expect(gyms).toEqual([expect.objectContaining({ title: 'Near Gym' })])
+  })
+})
